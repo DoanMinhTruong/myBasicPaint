7ac9bfc9bc093b44229ed24210489445f247b04c
DoanMinhTruong <meir.truong.2309@gmail.com>
2020-12-29 16:54:21 +0700

    1st myBasicPaint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import "./App.css";
+const App = () => {
 
-function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [size, setSize] = useState(5);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = size;
+    contextRef.current = context;
+
+  }, [size]);
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  }
+  const finishDrawing = () => {
+
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  }
+  const draw = ({ nativeEvent }) => {
+
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  }
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d")
+    context.fillStyle = "white"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+  }
+  const decreaseSize = () => {
+    console.log("de");
+    if (size > 1)
+      setSize(size - 1);
+
+    console.log(size);
+
+  }
+  const increaseSize = () => {
+    console.log("in");
+
+    setSize(size + 1);
+    console.log(size);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="btn">
+        <button onClick={clearCanvas}>Clear</button>
+        <button onClick={decreaseSize}>-</button>
+        <h1>{size}</h1>
+
+        <button onClick={increaseSize}>+</button>
+      </div>
+
+      <canvas
+
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
     </div>
+
   );
-}
+};
 
 export default App;
